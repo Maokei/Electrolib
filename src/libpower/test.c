170d89e5d43d3c8416f39f7dbc35a4902034d969
Maokei <rickard.joh@gmail.com>
2015-10-16 15:59:55 +0200

    Changed libpower test.c

--- src/libpower/test.c
+++ src/libpower/test.c
@@ -1,11 +1,20 @@
+/**
+ * @author Rickard Johansson
+ * @file test.c
+ * @brief Small test application for libpower.so
+ * */
 #include <stdio.h>
 #include "libpower.h"
 
 int main() {
     float x = 2;
-    float xx = 2;
+    float xx = 5;
     float test = 0;
     test = calc_power_r(x,xx);
-    printf("Hello world i'm alive! %f\n", test);
+    printf("libpower.so test calculations.");
+    printf("Calculating power with volt 2 and resistance 5:  %f\n", test);
+    xx = 4;
+    test = calc_power_i(x, xx);
+    printf("Calculating power with volt 2 and current 4: %f\n", test);
     return 0;
 }
